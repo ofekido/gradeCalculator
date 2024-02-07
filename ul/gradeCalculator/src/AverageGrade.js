51d394687e761ab0976311e313c8f2edc0db672b
Ido <ofekidodo@gmail.com>
2024-02-07 02:48:42 +0200

    Average Grade page update

--- ul/gradeCalculator/src/AverageGrade.js
+++ ul/gradeCalculator/src/AverageGrade.js
@@ -1,25 +1,36 @@
 // AverageGrade.js
+
 import React from 'react';
 import { Link } from 'react-router-dom'; // Import Link component
 import Header from './Header'; // Import Header component
 import './styles.css'; // Import styles for consistent styling
+import averageFormula from './Average-Formula.jpg'; // Import the image from your folder
 
 const AverageGrade = () => {
     const params = new URLSearchParams(window.location.search);
     const averageGrade = params.get('averageGrade');
 
     return (
-        <div className="average-grade-container">
+        <div>
             <Header /> {/* Include the Header component */}
-            <div className="content">
-                <h2>Average Grade</h2>
-                <p>The average grade for the selected student is: {averageGrade}</p>
-                <Link to="/">Back</Link> {/* Use Link component for navigation */}
+            <div className="average-grade-container">
+                <div className="content">
+                    <h2>Average Grade</h2>
+                    <p>The average grade for the selected student is: {averageGrade}</p>
+                    <Link to="/">Back</Link> {/* Use Link component for navigation */}
+                </div>
             </div>
             {/* Footer */}
             <footer className="footer">
                 <p>&copy; Ido and David</p>
             </footer>
+            {/* Explanation */}
+            <div className="explanation">
+                <h3>How to Calculate Grade Average</h3>
+                <p>To calculate the grade average, you add up all the grades and then divide the sum by the number of grades. This method is similar to calculating any other type of average.</p>
+                {/* Add the imported image */}
+                <img src={averageFormula} alt="Explanation Image" />
+            </div>
         </div>
     );
 }
